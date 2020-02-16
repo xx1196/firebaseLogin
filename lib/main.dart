39dd8d740bf4367dd942347a69330f23b5b92fbd
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2020-02-16 17:03:33 -0500

    Poniendo tema naranja en la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,9 @@ class MyApp extends StatelessWidget {
         'login': (BuildContext context) => LoginPage(),
         'home': (BuildContext context) => HomePage()
       },
+      theme: ThemeData(
+        primaryColor: Colors.orange
+      ),
     );
 
     return Provider(child: materialApp);
