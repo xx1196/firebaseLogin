ecaee31698aeae542cc6e6ad909b8ec7aee5cde6
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2020-02-16 13:00:03 -0500

    Se crea fondo morado del formulario

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -3,11 +3,76 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget body = Center(
-      child: Text('Login page'),
+    Widget body = Stack(
+      children: <Widget>[_makeBackGround(context)],
     );
     return Scaffold(
       body: body,
     );
   }
+}
+
+Widget _makeBackGround(BuildContext context) {
+  final size = MediaQuery.of(context).size;
+
+  final colorGradient = <Color>[
+    Color.fromRGBO(63, 63, 156, 1.0),
+    Color.fromRGBO(90, 70, 178, 1.0),
+  ];
+
+  final purpleBckGround = Container(
+    height: size.height * .4,
+    width: double.infinity,
+    decoration: BoxDecoration(gradient: LinearGradient(colors: colorGradient)),
+  );
+
+  final circle = Container(
+    width: 100.0,
+    height: 100.0,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Color.fromRGBO(255, 255, 255, 0.05)),
+  );
+
+  return Stack(
+    children: <Widget>[
+      purpleBckGround,
+      Positioned(
+        top: 90.0,
+        left: 30.0,
+        child: circle,
+      ),
+      Positioned(
+        top: -40.0,
+        right: -30.0,
+        child: circle,
+      ),
+      Positioned(
+        bottom: -50.0,
+        right: -10.0,
+        child: circle,
+      ),
+      Positioned(
+        bottom: 120.0,
+        right: 20.0,
+        child: circle,
+      ),
+      Positioned(
+        bottom: -50.0,
+        left: -20.0,
+        child: circle,
+      ),
+      Container(
+        padding: EdgeInsets.only(top: 80.0),
+        child: Column(
+          children: <Widget>[
+            Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
+            SizedBox(height: 10.0, width: double.infinity),
+            Text('XX1199',
+                style: TextStyle(color: Colors.white, fontSize: 25.0))
+          ],
+        ),
+      )
+    ],
+  );
 }
