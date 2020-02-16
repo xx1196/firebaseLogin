70c9bdad43fa9879f2e21a80fba776223ed43355
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2020-02-16 16:27:33 -0500

    Agregando clase para validar los formularios

---
+++ lib/src/bloc/login_bloc.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+class LoginBloc {
+  final _emailController = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();
+
+  Function(String) get changeEmail => _emailController.sink.add;
+
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  Stream<String> get emailStream => _emailController.stream;
+
+  Stream<String> get password => _passwordController.stream;
+
+  dispose() {
+    _emailController?.close();
+    _passwordController?.close();
+  }
+}
