2e0e76c4b34d878a39cda1cf1937a31f0b21595e
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2020-02-16 23:03:56 -0500

    Mantener la data de los streams después de un hot reload

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -2,17 +2,29 @@ import 'package:firebase_login/src/bloc/login_bloc.dart';
 export 'package:firebase_login/src/bloc/login_bloc.dart';
 import 'package:flutter/material.dart';
 
+export 'package:firebase_login/src/bloc/login_bloc.dart';
+
 class Provider extends InheritedWidget {
+  static Provider _currentInstance;
+
+  factory Provider({Key key, Widget child}) {
+    if (_currentInstance == null) {
+      _currentInstance = Provider._internal(
+        key: key,
+        child: child,
+      );
+    }
+    return _currentInstance;
+  }
+
   final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
 
   static LoginBloc of(BuildContext context) {
-    return context
-        .dependOnInheritedWidgetOfExactType<Provider>()
-        .loginBloc;
+    return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
   }
 }
